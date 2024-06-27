709a2af6da04dc16d4d0aa61867883a5ea76a510
Egba Frank <Egbafrank72@gmail.com>
2024-06-27 14:01:59 +0000

    Updated show command to output strings

--- console.py
+++ console.py
@@ -196,7 +196,7 @@ class HBNBCommand(cmd.Cmd):
 
         key = c_name + "." + c_id
         try:
-            print(storage.all()[key])
+            print(str(storage.all()[key]))
         except KeyError:
             print("** no instance found **")
 
