a91579eaed682f6401a023c13461720ad2eb43b2
tevn23 <mabulatt23@gmail.com>
2024-07-29 02:11:41 +0200

    Pep8 fixes

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -6,28 +6,32 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-	"""Return welcome message"""
-	return "Hello HBNB!"
+    """Return welcome message"""
+    return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-	"""Returns string hbnb"""
-	return "HBNB"
+    """Returns string hbnb"""
+    return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
-	"""Returns string c followed by any text"""
+    """Returns string c followed by any text"""
 
-	return "C " + text.replace('_',' ')
+    return "C " + text.replace('_', ' ')
+
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text):
-	"""Returns python followed by any string"""
-	return "Python " + text.replace('_', ' ')
+    """Returns python followed by any string"""
+    return "Python " + text.replace('_', ' ')
 
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
